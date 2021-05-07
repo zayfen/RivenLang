bf3ca1602b2ba2c3e354fd63b34e4e7208609668
zayfen <zhangyunfeng0101@gmail.com>
2021-05-08 00:06:51 +0800

    chore: commit code

---
+++ parser/src/parse_expr.rs
@@ -0,0 +1,24 @@
+//! 解析expression
+use crate::ast::Expression;
+use crate::parser::Parser;
+use crate::token::Token;
+
+pub fn parse_expr (parser: &mut Parser) -> Result<Expression, String> {
+  let token = parser.next();
+  match token {
+    Token::Id { name } => {
+      if parser.lookahead == Token::Equal {
+        parser.next();
+        Ok(parse_assign_expr(&mut parser, name).unwrap())
+      }
+    }
+  }
+
+  Err("parse_expr unknown error".to_owned())
+}
+
+fn parse_assign_expr (parser: &mut Parser, id: String) -> Result<Expression::AssignmentExpression, String> {
+  return Ok(Expression::AssignmentExpression {
+    
+  })
+}
