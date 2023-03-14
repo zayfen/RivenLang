69a94e43272104430e0d8d85de71fd64f5535b88
zayfen <zhangyunfeng@pudutech.com>
2023-03-14 09:27:27 +0800

    chore: add test_parse_program

--- parser/src/parse_program.rs
+++ parser/src/parse_program.rs
@@ -1,4 +1,4 @@
-use crate::{parser::Parser, ast::Program, token::Token, parse_stmt_list::parse_stmt_list};
+use crate::{parser::Parser, ast::Program, token::Token, parse_stmt_list::parse_stmt_list, parse_expression::parse_expression};
 
 
 pub fn parse_program(parser: &mut Parser) -> Program {
@@ -9,4 +9,13 @@ pub fn parse_program(parser: &mut Parser) -> Program {
   parser.eat_token(Token::RBrace);
 
   Program(stmt_list)
+}
+
+#[test]
+fn test_parse_program() {
+  let code = "program { if (1 + 2) { name = \"zayfen\"; foo(name); function foo2 (nage) {a = 1+2; b = 2+4; print(a,b);}}}";
+  let mut parser = Parser::new(code);
+  let program_stmt = parse_program(&mut parser);
+  println!("{:?}", program_stmt);
+  
 }
